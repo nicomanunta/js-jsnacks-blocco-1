2070bb0bb180c380f188bacabe7b2f0ff5ad1315
nicomanunta <nic.manun@gmail.com>
2023-11-24 15:36:31 +0100

    Esercizio TuttiFrutti completato aggiunti commenti

--- array-lista/js/script.js
+++ array-lista/js/script.js
@@ -4,18 +4,22 @@ console.log(frigorifero)
 // aggiungere "pesca" all'array "frigorifero"
 frigorifero.push("pesca")
 console.log(frigorifero)
-// verificare se "cocomero" è presente nell 'array "frigorifero"
-// utilizzare una variabile flag 
+// verificare se "cocomero" è presente nell 'array "frigorifero" tramite un ciclo for
+// per farlo utilizzare una variabile flag 
 let flag = false
 for ( let i = 1; i<frigorifero.length; i++){
+    // se presente la variabile flag diventa vera
     if("cocomero" == frigorifero[i]){
         flag =true
     }
 }
+// fuori dal ciclo stampare un messaggio che dice se il cocomero c'è o meno
+// se la variabile flag diventa vera, stampare in console questo
 if(flag){
     console.log("Trovato! Devo solo preparare il cocktail.")
 
 }
+// altrimenti quest'altro
 else {
     console.log("Oh no, devo uscire a comprare il cocomero!")
 }
